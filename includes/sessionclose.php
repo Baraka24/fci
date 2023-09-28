d9245e685869a49a355a4cf487f51898d310a309
Baraka24 <bkinywa24@gmail.com>
2023-09-28 16:42:14 +0200

    [ENH] UX & Backend parts

--- includes/sessionclose.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-if (isset($_GET['msg'])) {
-    header('location:../index.php?msg=False&info=Mot de Passe Incorrect');
-} else {
-    header('location:../index.php');
-}
